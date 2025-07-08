590c24a5617462da50063b971156480bda4a4cb9
Mark-Joseph-Quizzagan <quizzaganmarkjoseph347@gmail.com>
2025-07-08 09:16:35 +0800

    Update activirt1.js

--- activirt1.js
+++ activirt1.js
@@ -1,18 +1,14 @@
 // Variables Activity
 
-// Step 1: Create a variable named name and store your name
 let name = "Berting";
 
-// Step 2: Create a variable named whatDoYouWannaBecomeInYourLife and store "programmer"
 let whatDoYouWannaBecomeInYourLife = "chef";
 
-// Step 3: Create a variable named gender and store your gender
 let gender = "male";
 
-// Step 4: Create a variable named twitterHandle and store your Twitter handle
 let twitterHandle = "@Berting_dev";
 
-// Step 5: Log all the variables to the console
+//Log all the variables to the console
 console.log("Name: " + name);
 console.log("What I want to become: " + whatDoYouWannaBecomeInYourLife);
 console.log("Gender: " + gender);
