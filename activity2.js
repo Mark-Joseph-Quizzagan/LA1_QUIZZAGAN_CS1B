88c6e5f13cf55bc948f143154cf6d1c8ca26ee99
Mark-Joseph-Quizzagan <quizzaganmarkjoseph347@gmail.com>
2025-07-08 09:22:04 +0800

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,32 @@
+// Numbers Activity
+
+let firstFavNum = 96;
+
+let secondFavNum = 69;
+
+
+// Addition
+let sum = firstFavNum + secondFavNum;
+
+// Subtraction
+let difference = firstFavNum - secondFavNum;
+
+// Multiplication
+let product = firstFavNum * secondFavNum;
+
+// Division
+let quotient = firstFavNum / secondFavNum;
+
+// Modulus (remainder)
+let remainder = firstFavNum % secondFavNum;
+
+// Exponentiation (Power)
+let power = firstFavNum ** secondFavNum;
+
+//Log the results to the console
+console.log("Addition: " + sum);
+console.log("Subtraction: " + difference);
+console.log("Multiplication: " + product);
+console.log("Division: " + quotient);
+console.log("Modulus (Remainder): " + remainder);
+console.log("Exponentiation (Power): " + power);
